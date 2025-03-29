dda215027e1375306066970339fd3f9eef91fe48
Santino Cataldi <107449931+SrNanu@users.noreply.github.com>
2025-03-29 20:38:46 +0000

    Correccion de errores del test-11

--- practico_01/ejercicio_02.py
+++ practico_01/ejercicio_02.py
@@ -81,4 +81,3 @@ assert maximo_recursivo(1, 10, 5, -5) == 10
 assert maximo_recursivo(4, 9, 18, 6) == 18
 assert maximo_recursivo(24, 9, 18, 20) == 24
 assert maximo_recursivo(24, 9, 18, 30) == 30
-# NO MODIFICAR - FIN
