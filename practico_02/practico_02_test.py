f79c78562ea8b083b1b79333c4e9f678286a851a
SrNanu <107449931+SrNanu@users.noreply.github.com>
2025-04-01 18:17:02 -0300

    Correccion para pasar el test

--- practico_02/practico_02_test.py
+++ practico_02/practico_02_test.py
@@ -1,12 +1,11 @@
 def test_ejercicio_01():
     import ejercicio_01
 
+
 def test_ejercicio_02():
     import source.ejercicio_02
+
 
 def test_ejercicio_03():
     import source.controller.ejercicio_03
 
-test_ejercicio_01()
-test_ejercicio_02()
-test_ejercicio_03()
